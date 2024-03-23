2aedb1c89ed4956d71f03e420285f5780c06dc69
Jan Joos <janfrederic.joos@uzh.ch>
2024-03-23 19:25:01 +0100

    prettier used to format dashboard #2

--- src/components/views/Dashboard.tsx
+++ src/components/views/Dashboard.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import { api, handleError } from "helpers/api";
 import User from "models/User";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/Login.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
-import { Switch } from "../ui/switch"
-import { Card,  CardContent, CardDescription, 
-  CardFooter, CardHeader, CardTitle } from "../ui/card";
-import { Label } from "../ui/label"
-import { Progress } from "../ui/progress"
+import { Switch } from "../ui/switch";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Label } from "../ui/label";
+import { Progress } from "../ui/progress";
 import { Skeleton } from "../ui/skeleton";
 
-
-import '../../styles/views/Dashboard.scss'; // Assuming you have a CSS file for styles
+import "../../styles/views/Dashboard.scss"; // Assuming you have a CSS file for styles
 
 // Components
 const FriendList: React.FC = () => {
@@ -32,45 +37,58 @@ const FriendList: React.FC = () => {
 };
 
 const WelcomeMessage: React.FC = () => {
-    return (
-        <div className="welcome component">
-          <h1 className="welcome-title">Welcome back, Alberto!</h1>
-          <p>Your progress:</p>
-          <div className="mb-8"> {/* This adds a bottom margin. '8' can be adjusted to the desired space */}
-            <Progress value={35} />
+  return (
+    <div className="welcome component">
+      <h1 className="welcome-title">Welcome back, Alberto!</h1>
+      <p>Your progress:</p>
+      <div className="mb-8">
+        {" "}
+        {/* This adds a bottom margin. '8' can be adjusted to the desired space */}{" "}
+        <Progress value={35} />
+      </div>
+
+      <div className="current-trips component">
+        <h2>Current Trips</h2>
+        <div className="trip-container">
+          <div className="trip-info">
+            <div>Trip01 to Zürich, Platte</div>
+            <Button width="80px" height="35px" backgroundColor="#FFB703">
+              Info
+            </Button>
           </div>
-      
-          <div className="current-trips component">
-            <h2>Current Trips</h2>
-            <div className="trip-container">
-              <div className="trip-info">
-                <div>Trip01 to Zürich, Platte</div>
-                <Button width="80px" height="35px" backgroundColor="#FFB703">Info</Button>
-              </div>
-              <div className="trip-info">
-                <div>Trip02 to Zürich, HB</div>
-                <Button width="80px" height="35px" backgroundColor="#FFB703">Info</Button>
-              </div>
-              <div className="trip-info">
-                <div>Trip04 to Zürich, ETH Universitätsspital</div>
-                <Button width="80px" height="35px" backgroundColor="#FFB703">Info</Button>
-              </div>
-              <div className="trip-info">
-                <div>Trip05 to Zürich, ETH Universitätsspital</div>
-                <Button width="80px" height="35px" backgroundColor="#FFB703">Info</Button>
-              </div>
-              <div className="trip-info">
-                <div>Trip06 to Zürich, ETH Universitätsspital</div>
-                <Button width="80px" height="35px" backgroundColor="#FFB703">Info</Button>
-              </div>
-            </div>
+          <div className="trip-info">
+            <div>Trip02 to Zürich, HB</div>
+            <Button width="80px" height="35px" backgroundColor="#FFB703">
+              Info
+            </Button>
           </div>
-          <div className="create-button-container">
-              <Button width="150px" backgroundColor="#FB8500">CREATE TRIP</Button>
+          <div className="trip-info">
+            <div>Trip04 to Zürich, ETH Universitätsspital</div>
+            <Button width="80px" height="35px" backgroundColor="#FFB703">
+              Info
+            </Button>
+          </div>
+          <div className="trip-info">
+            <div>Trip05 to Zürich, ETH Universitätsspital</div>
+            <Button width="80px" height="35px" backgroundColor="#FFB703">
+              Info
+            </Button>
+          </div>
+          <div className="trip-info">
+            <div>Trip06 to Zürich, ETH Universitätsspital</div>
+            <Button width="80px" height="35px" backgroundColor="#FFB703">
+              Info
+            </Button>
           </div>
         </div>
-    );
-      
+      </div>
+      <div className="create-button-container">
+        <Button width="150px" backgroundColor="#FB8500">
+          CREATE TRIP
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 const NotificationsLog: React.FC = () => {
@@ -80,11 +98,11 @@ const NotificationsLog: React.FC = () => {
       {/* Placeholder content */}
       <div>
         <ol>
-            <li>11:34 - Michael updated trip members XXXXXX XXXXXX XXX</li>
-            <li>11:37 - Michael updated trip members XXXXXX XXXXXX XXX</li>
-            <li>11:39 - Michael updated trip members XXXXXX XXXXXX XXX</li>
+          <li>11:34 - Michael updated trip members XXXXXX XXXXXX XXX</li>
+          <li>11:37 - Michael updated trip members XXXXXX XXXXXX XXX</li>
+          <li>11:39 - Michael updated trip members XXXXXX XXXXXX XXX</li>
         </ol>
-        </div>
+      </div>
     </div>
   );
 };
@@ -113,49 +131,48 @@ const YourFavorites: React.FC = () => {
 };
 
 const FriendLeaderboard: React.FC = () => {
-    // This component will render the friend leaderboard
-    return (
-      <div className="friend-leaderboard component">
-        <h2>Friend-Leaderboard</h2>
-        {/* Placeholder content */}
-        <ol>
-          <li>Michael B.</li>
-          <li>Ulf Z.</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-          <li>Michael Banane</li>
-        </ol>
-      </div>
-    );
-  };
+  // This component will render the friend leaderboard
+  return (
+    <div className="friend-leaderboard component">
+      <h2>Friend-Leaderboard</h2>
+      {/* Placeholder content */}
+      <ol>
+        <li>Michael B.</li>
+        <li>Ulf Z.</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+        <li>Michael Banane</li>
+      </ol>
+    </div>
+  );
+};
 
-  // Main Dashboard component
-  const Dashboard2: React.FC = () => {
-    return (
-        <div className="dashboard">
-          <div className="column friend-list">
-            <FriendList />
-            <FriendLeaderboard />
-          </div>
-          <div className="column middle-column">
-            <WelcomeMessage />
-            <TripInvitations />
-          </div>
-          <div className="column notifications-favorites">
-            <NotificationsLog />
-            <YourFavorites />
-          </div>
-        </div>
-      );
-  };
+// Main Dashboard component
+const Dashboard2: React.FC = () => {
+  return (
+    <div className="dashboard">
+      <div className="column friend-list">
+        <FriendList />
+        <FriendLeaderboard />
+      </div>
+      <div className="column middle-column">
+        <WelcomeMessage />
+        <TripInvitations />
+      </div>
+      <div className="column notifications-favorites">
+        <NotificationsLog />
+        <YourFavorites />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard2;
-
 
 /* const Dashboard = () => {
   return (
