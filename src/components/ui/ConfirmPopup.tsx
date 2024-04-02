0287a01b016763f2c5fffd6d98a4571f69d5ac46
Jan Frédéric Joos <janfrederic.joos@uzh.ch>
2024-04-02 20:08:57 +0200

    added Delete User Profile and dynamic Dashboard #62

--- src/components/ui/ConfirmPopup.tsx
+++ src/components/ui/ConfirmPopup.tsx
@@ -1,36 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import "../../styles/ui/ConfirmPopup.scss";
 
 interface ConfirmPopupProps {
-    isOpen: boolean;
-    header: string;
-    info?: string;
-    children?: React.ReactNode;
+  isOpen: boolean;
+  header: string;
+  info?: string;
+  children?: React.ReactNode;
 }
 
-const ConfirmPopup: React.FC<ConfirmPopupProps> = ({ header, info, children, isOpen }) => {
-    if (!isOpen) return null;
+const ConfirmPopup: React.FC<ConfirmPopupProps> = ({
+  header,
+  info,
+  children,
+  isOpen,
+}) => {
+  if (!isOpen) return null;
 
-    return (
-        <div className="popup-overlay">
-            <div className="popup">
-                <h2 className="">{header}</h2>
-                <p>{info}</p>
-                <div className="button-container">
-                {children}
-                </div>
-            </div>
-        </div>
-    );
-
+  return (
+    <div className="popup-overlay">
+      <div className="popup">
+        <h2 className="">{header}</h2>
+        <p>{info}</p>
+        <div className="button-container">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ConfirmPopup;
 
 ConfirmPopup.propTypes = {
-    header: PropTypes.string.isRequired,
-    info: PropTypes.string,
-    isOpen: PropTypes.bool.isRequired,
-    children: PropTypes.node
+  header: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.node,
 };
