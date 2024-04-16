d9e8cc9977763c306435192968d2d6f14abcccd8
Livio Hartmann <livio.hartmann@uzh.ch>
2024-04-17 00:32:17 +0200

    Added the functionality to add a trip to favourite trips #52

--- src/components/ui/favourites.tsx
+++ src/components/ui/favourites.tsx
@@ -1,50 +1,83 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
+import { api, handleError } from "helpers/api";
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import "../../styles/ui/Favourites.scss";
 
-const StyledRating = styled(Rating)({
-  "& .MuiRating-iconFilled": {
-    color: "#ff6d75",
-  },
-  "& .MuiRating-iconHover": {
-    color: "#ff3d47",
-  },
-});
-
-const TripContainer = ({ id }) => {
-  return (
-    <div className="test">
-      <label>Trip {id}</label>
-      <StyledRating
-        //name="customized-color" // braucht es womöglich nicht
-        defaultValue={1} // Herzen sollen ausgefüllt sein
-        max={1}
-        precision={1}
-        // sorgt dafür dass die Form Herz ist
-        icon={<FavoriteIcon fontSize="inherit" />}
-        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
-      />
-    </div>
-  );
-};
-
-TripContainer.propTypes = {
-  id: PropTypes.number,
-};
-
 const Favourites = () => {
 
-  // hier alle favourites fetchen
-  const trips = ["Trip 1", "Trip 2", "Trip 3"];
+  const [favTrips, setFavTrips] = useState([]);
+  const token = localStorage.getItem("token");
+
+  const StyledRating = styled(Rating)({
+    "& .MuiRating-iconFilled": {
+      color: "#ff6d75",
+    },
+    "& .MuiRating-iconHover": {
+      color: "#ff3d47",
+    },
+  });
+
+  const handleClick = async (tripId) => {
+    try {
+      const response = await api.put(`/trips/${tripId}/favorites `, {}, {
+        headers: { Authorization: token },
+      });
+      
+    } catch (error) {
+      handleError(error)
+    }
+    finally {
+      fetchTrips();
+    }
+  }
+
+  const fetchTrips = async () => {
+    try {
+      const response = await api.get("/trips/favorites", {
+        headers: { Authorization: token },
+      });
+      setFavTrips(response.data);
+
+    } catch (error) {
+      handleError(error);
+    }
+  }
+
+  const TripContainer = ({ name, id }) => {
+    return (
+      <div className="favourites-container">
+        <label>{name}</label>
+        <StyledRating
+          defaultValue={1} 
+          max={1}
+          precision={1}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+          onMouseDown={() => handleClick(id)}
+          onTouchStart={() => handleClick(id)}
+        />
+      </div>
+    );
+  };
+
+  TripContainer.propTypes = {
+    name: PropTypes.string,
+    id: PropTypes.number
+  };
+
+  useEffect(() => {
+    fetchTrips()
+  }, []);
+
   return (
     <ul>
-      {trips.map((trip, index) => (
-        <li className="test" key={index}>
-          <TripContainer id={index}></TripContainer>
+      {favTrips.map((fav) => (
+        <li className="test" key={fav.id}>
+          <TripContainer name={fav.tripName} id={fav.id}></TripContainer>
         </li>
       ))}
     </ul>
