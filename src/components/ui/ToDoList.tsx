0519eb5f372ab2ff0ca5614c6d7697bdfd5c919a
Marc-hei <157007960+Marc-hei@users.noreply.github.com>
2024-04-18 17:45:44 +0200

    added snackbar / muialert to login, register and profile page

--- src/components/ui/ToDoList.tsx
+++ src/components/ui/ToDoList.tsx
@@ -195,7 +195,7 @@ const ToDoList = ({avatars, userId, setSnackbarMessage, setSnackbarSeverity, set
       </div>}
       {!isPopupOpen && <div className="Carousel button-holder">
         <Button backgroundColor={"white"} onClick={() => {setPopupOpen(true)}}>Add Item</Button>
-        <Button backgroundColor={"white"} onClick={() => {setEditMode(old => !old)}}>{editMode ? "Normal Mode" : "Edit Mode"}</Button>
+        <Button backgroundColor={"white"} onClick={() => {setEditMode(old => !old)}}>{editMode ? "Normal" : "Edit"}</Button>
       </div>}
       {!isPopupOpen && content}
     </div>
