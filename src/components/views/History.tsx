e8116800a21408ff474ffc2d870061f2b0b8cd14
Jan Joos <janfrederic.joos@uzh.ch>
2024-04-19 21:01:09 +0200

    added some bad scss for mobile tripOverview view #16

--- src/components/views/History.tsx
+++ src/components/views/History.tsx
@@ -1,35 +1,68 @@
+// For List
 import React, { useState, useEffect, useRef } from "react";
 import { api, handleError } from "helpers/api";
 import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import { Progress } from "../ui/progress";
 import LinearIndeterminate from "components/ui/loader";
-import "../../styles/views/FriendListPage.scss";
+import "../../styles/views/History.scss";
 import { Input } from "components/ui/input";
+
+// For Heart
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { styled } from "@mui/material/styles";
+import Rating from "@mui/material/Rating";
+
+const Heart = () => {
+  const token = localStorage.getItem("token");
+
+  const StyledRating = styled(Rating)({
+    "& .MuiRating-iconFilled": {
+      color: "#ff6d75",
+    },
+    "& .MuiRating-iconHover": {
+      color: "#ff3d47",
+    },
+  });
+
+  const handleClick = async (tripId) => {
+    try {
+      const response = await api.put(`/trips/${tripId}/favorites `, {}, {
+        headers: { Authorization: token },
+      });
+      
+    } catch (error) {
+      handleError(error)
+    }
+  }
+
+  return (
+    <StyledRating
+          defaultValue={1} 
+          max={1}
+          precision={1}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+          onMouseDown={() => handleClick(id)}
+          onTouchStart={() => handleClick(id)}
+        />
+  )
+}
 
 const History = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  const [friendList, setFriendList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [historyTrips, setHistoryTrips] = useState([]);
-
-  // zu fetchTrips umwandeln    
+   
   const fetchHistory = async () => {
     try {
-      const userdata = await api.get("/users", {
-        headers: { Authorization: token },
-      });
-
-      console.log("Halllllo");
-      console.log(userdata.data.id);
-
       const response = await api.get(`/trips/history`, {
         headers: { Authorization: token },
       });
-
       setHistoryTrips(response.data);
-      console.log("History of Trips: ", response.data);
+
     } catch (error) {
       handleError(error);
     }
@@ -39,11 +72,12 @@ const History = () => {
     navigate("/dashboard");
   };
 
-  useEffect(() => {
-    fetchHistory();
-  }, [])
+  const showTripOverview = (id) => {
+    navigate(`/tripOverview/${id}`);
+  };
 
   useEffect(() => {
+    fetchHistory();
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1500); // Show loader for x seconds
@@ -55,21 +89,31 @@ const History = () => {
   }
 
   return (
-    <div className="friend-list-page">
+    <div className="history-list-page">
       <h1>Your Trip History</h1>
       {historyTrips.length > 0 ? (
-          <ul className="friend-list">
+          <ul className="history-list">
           {historyTrips.map((trip) => (
-              <li key={trip.id} className="friend">
-                <span className="name">{trip.tripName}</span>
+              <li key={trip.id} className="trip">
+                <span className="name" onClick={() => showTripOverview(trip.id)}>{trip.tripName}</span>
               </li>
           ))}
           </ul>
       ) : (
           <div className="no-friends-message">
-          No trips yet!
+          No trips completed yet!
           </div>
       )}
+      <div className="button-container">
+        <Button
+          backgroundColor="#FFB703"
+          color="black"
+          onClick={handleBackClick}
+          >
+          Back to Dashboard
+        </Button>
+      </div>
+      
     </div>
   );
 };
