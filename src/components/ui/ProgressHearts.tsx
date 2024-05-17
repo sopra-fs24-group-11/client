39ca0a77116865e4620da0c3c5aaccd098998595
Livio Hartmann <livihartmann@gmail.com>
2024-05-17 22:54:22 +0200

    Added progress bar for friend in form of 5 hearts with double quotes #64

--- src/components/ui/ProgressHearts.tsx
+++ src/components/ui/ProgressHearts.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import Rating from '@mui/material/Rating';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Rating from "@mui/material/Rating";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import PropTypes from "prop-types";
 
 const ProgressHearts = ({ points }) => {
   
   const StyledRating = styled(Rating)({
-    '& .MuiRating-iconFilled': {
-      color: '#ff6d75',
+    "& .MuiRating-iconFilled": {
+      color: "#ff6d75",
     },
-    '& .MuiRating-iconHover': {
-      color: '#ff3d47',
+    "& .MuiRating-iconHover": {
+      color: "#ff3d47",
     },
   });
 
