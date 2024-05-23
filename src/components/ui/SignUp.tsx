a8a5dc29808fb12b699c3dc86e13b05c493f1087
Jan Frédéric Joos <janfrederic.joos@uzh.ch>
2024-05-23 13:41:04 +0200

    fixed the SignUp form

--- src/components/ui/SignUp.tsx
+++ src/components/ui/SignUp.tsx
@@ -1,24 +1,31 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "helpers/api";
+import PropTypes from "prop-types";
 
-interface SignInFormProps {
-  alertUser: (type: string, message: string, error?: any) => void;
-}
+SignUpForm.propTypes = {
+  alertUser: PropTypes.func.isRequired,
+};
 
-interface State {
-  username: string;
-  password: string;
-}
-
-function SignInForm({ alertUser }: SignInFormProps) {
+function SignUpForm({ alertUser }) {
   const navigate = useNavigate();
-  const [state, setState] = useState<State>({
+  const [state, setState] = React.useState({
     username: "",
+    email: "",
+    birthday: "",
     password: "",
+    password2: "",
   });
 
-  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+  const handleBirthdayChange = (event) => {
+    const { value } = event.target;
+    setState({
+      ...state,
+      birthday: value,
+    });
+  };
+
+  const handleChange = (evt) => {
     const value = evt.target.value;
     setState({
       ...state,
@@ -26,52 +33,79 @@ function SignInForm({ alertUser }: SignInFormProps) {
     });
   };
 
-  const handleOnSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+  const handleOnSubmit = async (evt) => {
     evt.preventDefault();
-    const { username, password } = state;
+    const { username, email, birthday, password, password2 } = state;
 
     try {
-      const requestBody = JSON.stringify({ username, password });
-      const response = await api.post("/users/login", requestBody);
+      const requestBody = JSON.stringify({
+        username,
+        password,
+        password2,
+        birthday,
+        email,
+      });
+      const response = await api.post("/users/register", requestBody);
 
       localStorage.setItem("token", response.data);
       navigate("/dashboard");
     } catch (error) {
-      alertUser("error", "Benutzername oder Passwort ist falsch.", error);
+      alertUser("error", "", error);
     }
   };
 
   return (
-    <div className="form-container sign-in-container">
+    <div className="form-container sign-up-container">
       <form className="Register-Login-form" onSubmit={handleOnSubmit}>
-        <h1 className="h1_reg">Mit Profil anmelden</h1>
+        <h1 className="h1_reg">Account erstellen</h1>
+
+        <span className="span_reg">
+          Bitte fülle das untenstehende Formular aus um einen Account zu
+          erstellen.
+        </span>
         <input
           className="inputreg"
           type="text"
-          placeholder="Benutzername"
           name="username"
           value={state.username}
           onChange={handleChange}
+          placeholder="Benutzername"
+        />
+        <input
+          className="inputreg"
+          type="email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          placeholder="Email-Adresse"
+        />
+        <input
+          className="inputreg"
+          type="date"
+          placeholder="enter here.."
+          value={state.birthday}
+          onChange={handleBirthdayChange}
         />
         <input
           className="inputreg"
           type="password"
           name="password"
-          placeholder="Passwort"
           value={state.password}
           onChange={handleChange}
+          placeholder="Passwort"
         />
-        <a
-          className="a_reg"
-          href="#"
-          onClick={() => alertUser("warning", "Passwort kann zum aktuellen Zeitpunkt nicht zurückgesetzt werden. Erstellen Sie einen neuen Account.")}
-        >
-          Passwort vergessen?
-        </a>
-        <button className="buttonreg">Jetzt Anmelden</button>
+        <input
+          className="inputreg"
+          type="password"
+          name="password2"
+          value={state.password2}
+          onChange={handleChange}
+          placeholder="Passwort bestätigen"
+        />
+        <button className="buttonreg">Jetzt registrieren</button>
       </form>
     </div>
   );
 }
 
-export default SignInForm;
+export default SignUpForm;
